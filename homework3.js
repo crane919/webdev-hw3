56463e33b5a6839bfd1a70066d8da7f1d7075cda
Maya Cranor <mcranor@olin.edu>
2024-03-08 12:20:44 -0500

    Update mvc code

--- homework3.js
+++ homework3.js
@@ -79,36 +79,30 @@ class Model {
 
   // Actions.
   actionChooseCard(card) {
-    console.log(card, "cardd")
-    console.log(this.board[card].state)
-    let cardState = this.board[card].state
+    cardState = this.board[card].state
+    
     // only click on hidden cards
     if (cardState == 0){
       if (this.pos1 == null){ // first card
         this.board[card].state = 1
-        // BoardView.update_card(card)
-        // this.changedCardSubscribers.forEach(card)
         this.pos1 = card
       } else { // second card
         this.board[card].state = 1
-        //this.changedCardSubscribers.forEach(card)
         this.pos2 = card
-        // check match
-        if (this.board[this.pos1].card == this.board[this.pos2].card){
-          this.board[this.pos1].state = 2
-          this.board[this.pos2].state = 2
-          //this.changedCardSubscribers.forEach(card)
-          //this.changedCardSubscribers.forEach(card)
-        } else {
-          //wait one second
-          this.board[this.pos1].state = 0
-          this.board[this.pos2].state = 0
-          //this.changedCardSubscribers.forEach(card)
-          //this.changedCardSubscribers.forEach(card)
-        }
-        this.pos1 = null
-        this.pos2 = null
-        // check game finished
+        // // check match move??
+        // if (this.board[this.pos1].card == this.board[this.pos2].card){
+        //   this.board[this.pos1].state = 2
+        //   this.board[this.pos2].state = 2
+
+        // } else {
+        //   //wait one second
+        //   this.board[this.pos1].state = 0
+        //   this.board[this.pos2].state = 0
+
+        // }
+        // this.pos1 = null
+        // this.pos2 = null
+        // // check game finished
       }  
     }  
   }
@@ -136,7 +130,7 @@ class BoardView {
     // Create and append the card elements
     for (let i = 0; i < 12; i++) {
       const cardDiv = create('div',  {'class':'card hidden', 'data_value':this.model.board[i].card}, i) 
-      cardDiv.addEventListener('click', () => { this.model.actionChooseCard(i)} )
+      // cardDiv.addEventListener('click', () => { this.model.actionChooseCard(i)} )
       gameGrid.appendChild(cardDiv)
     }
     document.body.appendChild(gameGrid)
@@ -144,6 +138,42 @@ class BoardView {
 
   root() {
     return this.elt
+  }
+
+  
+
+}
+
+
+class CardController {
+  // when board view inits assign one controller per card
+  constructor(m) {
+    this.model = m
+    console.log('Set up card controller here')
+    for (let i = 0; i < 12; i++) {
+      elt(id).addEventListener('click', () => { this.someFunc(i)} )
+    }
+  }
+
+
+  someFunc(i){
+    this.model.actionChooseCard(i) // call choose card
+    // either pos1 or pos 2 will be updated
+    this.update_card(i) // update_card
+    // check match logic time
+    if (this.model.pos1 != null && this.model.pos2 != null){
+      if ()
+
+    }
+
+
+    // if match  - call_chose to update model
+    // call update card - both cards
+    // if not match 
+      // wait two seconds
+      // update model
+      // update card
+
   }
 
   update_card(card){
@@ -163,15 +193,7 @@ class BoardView {
     }
   }
 
-}
 
-
-class CardController {
-  // when board view inits assign one controller per card
-  constructor(m) {
-    this.model = m
-    console.log('Set up card controller here')
-  }
   // subscription to model  to figure out what card was clicked
   // published line back to model
     // invoke choose card
