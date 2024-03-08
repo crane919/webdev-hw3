f99b26fc0fda09d3fd84585761dd613b0adf0f9f
Maya Cranor <mcranor@olin.edu>
2024-03-08 14:22:27 -0500

    Finished question 1

--- homework3.js
+++ homework3.js
@@ -11,6 +11,7 @@ window.onload = () => {
   new BoardView(model)
   new RoundView(model)
   new FinishedView(model)
+  new CardController(model)
   new NewGameController(model)
 }
 
@@ -79,8 +80,7 @@ class Model {
 
   // Actions.
   actionChooseCard(card) {
-    cardState = this.board[card].state
-    
+    let cardState = this.board[card].state
     // only click on hidden cards
     if (cardState == 0){
       if (this.pos1 == null){ // first card
@@ -89,22 +89,17 @@ class Model {
       } else { // second card
         this.board[card].state = 1
         this.pos2 = card
-        // // check match move??
-        // if (this.board[this.pos1].card == this.board[this.pos2].card){
-        //   this.board[this.pos1].state = 2
-        //   this.board[this.pos2].state = 2
-
-        // } else {
-        //   //wait one second
-        //   this.board[this.pos1].state = 0
-        //   this.board[this.pos2].state = 0
-
-        // }
-        // this.pos1 = null
-        // this.pos2 = null
-        // // check game finished
       }  
-    }  
+    } else if (cardState == 1){
+      // check how two chosen cards should be resolved
+      if (this.board[this.pos1].card == this.board[this.pos2].card){
+        this.board[this.pos1].state = 2
+        this.board[this.pos2].state = 2
+      } else {
+        this.board[this.pos1].state = 0
+        this.board[this.pos2].state = 0
+      }
+    }
   }
 
   actionNewGame() {
@@ -135,12 +130,7 @@ class BoardView {
     }
     document.body.appendChild(gameGrid)
   }
-
-  root() {
-    return this.elt
-  }
-
-  
+ 
 
 }
 
@@ -149,54 +139,65 @@ class CardController {
   // when board view inits assign one controller per card
   constructor(m) {
     this.model = m
-    console.log('Set up card controller here')
+    this.shape_mapping = {"0":"url('shapes/red-circ.svg')", "1":"url('shapes/blu-circ.svg')", "2":"url('shapes/grn-circ.svg')", "3":"url('shapes/red-rect.svg')", "4":"url('shapes/blu-rect.svg')", "5":"url('shapes/grn-rect.svg')"}
+    console.log('Set up card controller here aaaaaaaaaa')
     for (let i = 0; i < 12; i++) {
-      elt(id).addEventListener('click', () => { this.someFunc(i)} )
+      elt(i).addEventListener('click', () => { this.handleClick(i)} )
     }
   }
 
-
-  someFunc(i){
-    this.model.actionChooseCard(i) // call choose card
+  handleClick(card){
+    console.log("hello i was clicked", card)
+    this.model.actionChooseCard(card) // call choose card
     // either pos1 or pos 2 will be updated
-    this.update_card(i) // update_card
+    this.update_card(card) // update_card
     // check match logic time
     if (this.model.pos1 != null && this.model.pos2 != null){
-      if ()
-
+      // check match
+      this.model.actionChooseCard(card) //update model array
+      if (this.model.board[this.model.pos1].card == this.model.board[this.model.pos2].card) {
+        // this a match
+        this.update_card(this.model.pos1)
+        this.update_card(this.model.pos2)
+        this.model.pos1 = null
+        this.model.pos2 = null        
+      } else {
+        // this not a match
+        console.log(this.model.board)
+        setTimeout(() => {
+          this.update_card(this.model.pos1)
+          this.update_card(this.model.pos2)
+          this.model.pos1 = null
+          this.model.pos2 = null
+        }, 1000)
+      }
+      
     }
-
-
-    // if match  - call_chose to update model
-    // call update card - both cards
-    // if not match 
-      // wait two seconds
-      // update model
-      // update card
-
   }
 
   update_card(card){
     console.log("hello update card")
-    let eltCard = document.getElementById(card);
-
-    eltCard.classList.remove("hidden", "chosen", "revealed")
+    console.log(card, "this is card")
+    elt(card).classList.remove("hidden", "chosen", "revealed")
     switch(this.model.board[card].state){
       case 0:
-        eltCard.classList.add("hidden")
+        elt(card).classList.add("hidden")
+        console.log("noneeee")
+        elt(card).style.backgroundImage = 'none'
         break
       case 1:
-        eltCard.classList.add("chosen")
+        elt(card).classList.add("chosen")
+        elt(card).classList.add("revealed")
+        elt(card).style.backgroundImage = this.shape_mapping[this.model.board[card].card];
         break
       case 2:
-        eltCard.classList.add("revealed")
+        elt(card).classList.add("revealed")
+        console.log(this.shape_mapping)
+        elt(card).style.backgroundImage = this.shape_mapping[this.model.board[card].card];
+
     }
   }
 
-
-  // subscription to model  to figure out what card was clicked
-  // published line back to model
-    // invoke choose card
 }
 
 
